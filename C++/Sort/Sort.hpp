8ba9730ea6623bff880b8c4f64a3f008d63c3332
Mmmmmmi <812632545@qq.com>
2019-08-01 23:11:55 +0800

    update

--- C++/Sort/Sort.hpp
+++ C++/Sort/Sort.hpp
@@ -1,4 +1,4 @@
-pragma once
+#pragma once
 /*
  * 七大排序
  * 冒泡
@@ -10,24 +10,40 @@ pragma once
  * 归并
  */
 
-template <class T>
-void Swap(T &left, T &right)
-{
-	T temp = left;
-	left = right;
-	right = temp;
-}
+#include <iostream>
+#include <vector>
 
-
-template<class RandomIt, class comp>
+template<class T>
 class MySort
 {
+    typedef typename std::vector<T>::iterator Iterator;
 public:
-    static void BubbleSort();
+    static void BubbleSort(T* left, T* right);
+    static void BubbleSort(Iterator left, Iterator right);
 	static void SelectSort();
 	static void InsertSort();
 	static void ShellSort();
 	static void HeapSort();
 	static void QuickSort();
 	static void MerageSort();
+
+    template<class Compare>
+    static void BubbleSort(T* left, T* right, Compare comp);
+
+    template<class Compare>
+    static void BubbleSort(Iterator left, Iterator right, Compare comp);
+
 };
+
+template<class T>
+void MySort<T>::BubbleSort(T* left, T* right)
+{
+    BubbleSort(left, right, std::less<T>());
+    return;
+}
+
+template<class T, class Compare>
+void MySort<T, Compare>::BubbleSort(T* left, T* right, Compare comp)
+{
+    return;
+}
--- C++/Sort/test.cc
+++ C++/Sort/test.cc
@@ -6,10 +6,31 @@
  ************************************************************************/
 
 
-#include "Sort.hpp":
+#include "Sort.hpp"
 #include <iostream>
 using namespace std;
+
+template<class T>
+class comp
+{
+    bool operator()(T& left, T& right) const
+    {
+        if (left >= right)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
 int main()
 {
+    int a[] = {1, 2, 3, 4};
+    MySort<int, comp<int>()> m;
+    //m.BubbleSort(1, 2, comp<int>());
+    m.BubbleSort(a, a + 1);
     return 0;
 }
